2b9938c864a7f75763878a2ea4cc661f2146ff20
Nia Bakova <69983201+niavb@users.noreply.github.com>
2021-01-17 22:27:36 +0200

    Step 3 - Initial Functionality

--- HabitTracker.java
+++ HabitTracker.java
@@ -3,6 +3,7 @@ package project;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 
 import java.awt.event.*;
 
@@ -38,6 +39,10 @@ public class HabitTracker {
 				return false;
 			}
 		};
+		
+		habitsTable.setRowSelectionAllowed(false);
+		//disable column moving
+		habitsTable.getTableHeader().setReorderingAllowed(false);
 
 		habitsTableModel.addColumn("Days");
 		for (int i = 0; i < 7; i++) {
@@ -53,13 +58,13 @@ public class HabitTracker {
 		GridBagConstraints gbc = new GridBagConstraints();
 
 		// drop menu
-		String[] chooseHabit = { "HABIT 1", "HABIT 2" };
-		JComboBox<String> cb = new JComboBox<String>(chooseHabit);
+		String[] chooseHabit = { "Choose habit." };
+		JComboBox<String> chooseHabitDropMenu = new JComboBox<String>(chooseHabit);
 		gbc.gridx = 1;
 		gbc.gridy = 1;
 		gbc.gridwidth = 2;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-		rightPanel.add(cb, gbc);
+		rightPanel.add(chooseHabitDropMenu, gbc);
 
 		JButton doneButton = new JButton("Done");
 		gbc.gridx = 2;
@@ -67,6 +72,25 @@ public class HabitTracker {
 		gbc.gridwidth = 1;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		rightPanel.add(doneButton, gbc);
+		
+		doneButton.addActionListener(new ActionListener() {		
+			public void actionPerformed(ActionEvent e) {
+				
+				String chosenHabit = chooseHabitDropMenu.getSelectedItem().toString();
+				for(int i=0; i<habitsTableModel.getRowCount(); i++) {
+					if(habitsTable.getValueAt(i, 0)==chosenHabit) {
+						for(int j=0; j<habitsTableModel.getColumnCount()-1; j++) {
+							if(habitsTable.getValueAt(i, j+1)==null) {
+								habitsTableModel.setValueAt("done", i, j+1);
+								break;
+							}
+						}
+						break;
+					}
+				}
+				
+			}
+		});
 
 		JButton missedButton = new JButton("Missed");
 		gbc.gridx = 1;
@@ -74,6 +98,25 @@ public class HabitTracker {
 		gbc.gridwidth = 1;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		rightPanel.add(missedButton, gbc);
+		
+		missedButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String chosenHabit = chooseHabitDropMenu.getSelectedItem().toString();
+				for(int i=0; i<habitsTableModel.getRowCount(); i++) {
+					if(habitsTable.getValueAt(i, 0)==chosenHabit) {
+						for(int j=0; j<habitsTableModel.getColumnCount()-1; j++) {
+							if(habitsTable.getValueAt(i, j+1)==null) {
+								habitsTableModel.setValueAt("missed", i, j+1);
+								break;
+							}
+						}
+						break;
+					}
+				}
+				
+			}
+		});
 
 		JTextField addHabit = new JTextField();
 		gbc.gridx = 1;
@@ -88,6 +131,17 @@ public class HabitTracker {
 		gbc.gridwidth = 2;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		rightPanel.add(addButton, gbc);
+		
+		addButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String habitToAdd = addHabit.getText();
+				habitsTableModel.insertRow(habitsTableModel.getRowCount(), new Object[] {habitToAdd});
+				chooseHabitDropMenu.addItem(habitToAdd);
+				addHabit.setText(null);
+				
+			}
+		});
 
 		frame.setVisible(true);
 
