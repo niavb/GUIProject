217234371c3f83ca8967e95d7ca0dcf2583797d7
Nia Bakova <69983201+niavb@users.noreply.github.com>
2021-01-24 15:39:33 +0200

    Step 5 TODO -> DONE

--- HabitTracker.java
+++ HabitTracker.java
@@ -1,13 +1,58 @@
 package project;
 
 import java.awt.*;
+
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
-
+import javax.swing.table.*;
 import java.awt.event.*;
+import java.io.*;
+import java.util.*;
 
-public class HabitTracker {
+public class HabitTracker extends JFrame {
+
+	// store table in file
+	public static void storeTableModel(DefaultTableModel model) {
+		ObjectOutputStream oos = null;
+		try {
+			oos = new ObjectOutputStream(new FileOutputStream("savedProgress"));
+			oos.writeObject(model.getDataVector());
+			oos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// store the ArrayList with habits from drop menu (JComboBox) in file
+	public static void storeDropMenuHabits(ArrayList<String> al) {
+		try {
+			FileWriter fw = new FileWriter("savedDropMenuHabits");
+			for (String habit : al)
+				fw.write(habit + System.lineSeparator());
+			fw.close();
+		} catch (IOException i) {
+			i.printStackTrace();
+		}
+	}
+
+	// method for the action of buttons DONE and MISSED
+	public static void checkingButtionsAction(String check, JComboBox<String> cb, DefaultTableModel model) {
+		String chosenHabit = cb.getSelectedItem().toString();
+		// going through every row in the first column of the table
+		for (int i = 0; i < model.getRowCount(); i++) {
+			if (model.getValueAt(i, 0).equals(chosenHabit)) {
+				// going through every cell in the selected row
+				for (int j = 0; j < model.getColumnCount() - 1; j++) {
+					// finding the first empty cell of the row
+					if (model.getValueAt(i, j + 1) == null) {
+						model.setValueAt(check, i, j + 1);
+						break;
+					}
+				}
+				break;
+			}
+		}
+		storeTableModel(model);
+	}
 
 	public static void main(String[] args) {
 
@@ -33,15 +78,38 @@ public class HabitTracker {
 		leftPanel.add(habitTrackerLabel);
 
 		// Table
+		// using DefaultTableModel for its methods like addRow(), addColumn()...
 		DefaultTableModel habitsTableModel = new DefaultTableModel();
 		JTable habitsTable = new JTable(habitsTableModel) {
+
+			// make the table cells not editable from user (only from buttons in rightPanel)
 			public boolean isCellEditable(int data, int colums) {
 				return false;
 			}
+
+			// set color of missed/done cells
+			public Component prepareRenderer(TableCellRenderer r, int row, int column) {
+				Component cell = super.prepareRenderer(r, row, column);
+				Object value = getModel().getValueAt(row, column);
+				if (value != null) {
+					if (value.equals("missed")) {
+						cell.setBackground(Color.red);
+					} else if (value.equals("done")) {
+						cell.setBackground(Color.green);
+					} else {
+						cell.setBackground(Color.white);
+					}
+				} else {
+					cell.setBackground(Color.white);
+				}
+				return cell;
+			}
+
 		};
-		
+
+		// disables selecting whole row when certain cell is clicked on
 		habitsTable.setRowSelectionAllowed(false);
-		//disable column moving
+		// disables column moving
 		habitsTable.getTableHeader().setReorderingAllowed(false);
 
 		habitsTableModel.addColumn("Days");
@@ -52,19 +120,48 @@ public class HabitTracker {
 		habitsTableModel.insertRow(0, new Object[] { "Habits" });
 		leftPanel.add(new JScrollPane(habitsTable));
 
+		// get stored table from file
+		ObjectInputStream ois = null;
+		try {
+			ois = new ObjectInputStream(new FileInputStream("savedProgress"));
+			Vector data = (Vector) ois.readObject();
+			Vector<Object> cols = new Vector<>();
+			cols.add("Days");
+			for (int i = 1; i < habitsTableModel.getColumnCount(); i++) {
+				cols.add(i);
+			}
+			habitsTableModel.setDataVector(data, cols);
+		} catch (Exception e) {
+		}
+
 		// right side
-		// layout
+		// for layout setting
 		rightPanel.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 
-		// drop menu
-		String[] chooseHabit = { "Choose habit." };
-		JComboBox<String> chooseHabitDropMenu = new JComboBox<String>(chooseHabit);
+		// drop-down menu
+		ArrayList<String> dropDownMenuHabits = new ArrayList<String>();
+		// get stored habits from file "savedDropMenuHabits" to the ArrayList
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("savedDropMenuHabits"));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				dropDownMenuHabits.add(line);
+			}
+			reader.close();
+		} catch (Exception e) {
+		}
+		JComboBox<String> dropDownMenu = new JComboBox<String>();
+		// add ArrayList to drop menu (combo box)
+		for (int i = 0; i < dropDownMenuHabits.size(); i++) {
+			dropDownMenu.addItem(dropDownMenuHabits.get(i));
+		}
+		// set layout
 		gbc.gridx = 1;
 		gbc.gridy = 1;
 		gbc.gridwidth = 2;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-		rightPanel.add(chooseHabitDropMenu, gbc);
+		rightPanel.add(dropDownMenu, gbc);
 
 		JButton doneButton = new JButton("Done");
 		gbc.gridx = 2;
@@ -72,23 +169,10 @@ public class HabitTracker {
 		gbc.gridwidth = 1;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		rightPanel.add(doneButton, gbc);
-		
-		doneButton.addActionListener(new ActionListener() {		
+
+		doneButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				String chosenHabit = chooseHabitDropMenu.getSelectedItem().toString();
-				for(int i=0; i<habitsTableModel.getRowCount(); i++) {
-					if(habitsTable.getValueAt(i, 0)==chosenHabit) {
-						for(int j=0; j<habitsTableModel.getColumnCount()-1; j++) {
-							if(habitsTable.getValueAt(i, j+1)==null) {
-								habitsTableModel.setValueAt("done", i, j+1);
-								break;
-							}
-						}
-						break;
-					}
-				}
-				
+				checkingButtionsAction("done", dropDownMenu, habitsTableModel);
 			}
 		});
 
@@ -98,23 +182,10 @@ public class HabitTracker {
 		gbc.gridwidth = 1;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		rightPanel.add(missedButton, gbc);
-		
+
 		missedButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				String chosenHabit = chooseHabitDropMenu.getSelectedItem().toString();
-				for(int i=0; i<habitsTableModel.getRowCount(); i++) {
-					if(habitsTable.getValueAt(i, 0)==chosenHabit) {
-						for(int j=0; j<habitsTableModel.getColumnCount()-1; j++) {
-							if(habitsTable.getValueAt(i, j+1)==null) {
-								habitsTableModel.setValueAt("missed", i, j+1);
-								break;
-							}
-						}
-						break;
-					}
-				}
-				
+				checkingButtionsAction("missed", dropDownMenu, habitsTableModel);
 			}
 		});
 
@@ -131,15 +202,18 @@ public class HabitTracker {
 		gbc.gridwidth = 2;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		rightPanel.add(addButton, gbc);
-		
+
 		addButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				String habitToAdd = addHabit.getText();
-				habitsTableModel.insertRow(habitsTableModel.getRowCount(), new Object[] {habitToAdd});
-				chooseHabitDropMenu.addItem(habitToAdd);
+				habitsTableModel.insertRow(habitsTableModel.getRowCount(), new Object[] { habitToAdd });
+				dropDownMenu.addItem(habitToAdd);
+				dropDownMenuHabits.add(habitToAdd);
 				addHabit.setText(null);
-				
+				storeTableModel(habitsTableModel);
+				storeDropMenuHabits(dropDownMenuHabits);
+
 			}
 		});
 
